5d2a61678769eeb94795a2c222bc15f5667587e1
Douglas Dreer <douglasdreer@outlook.com.br>
2025-10-02 08:54:42 -0300

    feat(project-setup): Cria estrutura inicial do projeto com Spring Boot, PostgreSQL, e configurações básicas

---
+++ src/main/kotlin/br/com/tcg/pokemon/pokemontradecardgame/PokemonTradeCardGameApplication.kt
@@ -0,0 +1,11 @@
+package br.com.tcg.pokemon.pokemontradecardgame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokemonTradeCardGameApplication
+
+fun main(args: Array<String>) {
+    runApplication<PokemonTradeCardGameApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/tcg/pokemon/pokemontradecardgame/PokemonTradeCardGameApplicationTests.kt
@@ -0,0 +1,14 @@
+package br.com.tcg.pokemon.pokemontradecardgame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+
+class PokemonTradeCardGameApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
