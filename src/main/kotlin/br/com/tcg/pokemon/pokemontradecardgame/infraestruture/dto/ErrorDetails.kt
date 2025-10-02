8897d5889c43a0136d7913ec7959c06039206f39
Douglas Dreer <douglasdreer@outlook.com.br>
2025-10-02 13:16:57 -0300

    feat(security): implementa handlers para erros de autenticação e autorização
    Adiciona `SecurityExceptionHandler` para gerenciar exceções de autenticação e autorização, retornando respostas JSON detalhadas. Também inclui a classe `ErrorDetails` para padronizar mensagens de erro.

---
+++ src/main/kotlin/br/com/tcg/pokemon/pokemontradecardgame/infraestruture/dto/ErrorDetails.kt
@@ -0,0 +1,29 @@
+package br.com.tcg.pokemon.pokemontradecardgame.infraestruture.dto
+
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+
+
+data class ErrorDetails(
+    val status: Int,
+    val error: String,
+    val message: String? = null
+) {
+    companion object {
+        private val gson = Gson()
+    }
+
+    /**
+     * Serializes the [ErrorDetails] instance to a JSON string.
+     *
+     * @return A JSON string representation of the object.
+     * @throws JsonSyntaxException If serialization fails due to invalid data.
+     */
+    fun toJSON(): String {
+        return try {
+            gson.toJson(this)
+        } catch (e: JsonSyntaxException) {
+            throw JsonSyntaxException("Failed to serialize ErrorDetails to JSON: ${e.message}")
+        }
+    }
+}
